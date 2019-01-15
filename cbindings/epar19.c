cd99c2da93d8a9cb68f916c04b8259c951bfd786
Andrew Danner <abdanner@gmail.com>
2019-01-14 22:08:01 -0500

    api change in cbindings

--- cbindings/epar19.c
+++ cbindings/epar19.c
@@ -31,24 +31,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define DEFAULT_DIM 50
-#define DEFAULT_INTERs 0
+#define DEFAULT_DIM 100
+#define DEFAULT_INTERS 150
 #define DEFAULT_TIDS 4
 #define SLEEPYTIME 2000
 
-/* (un)comment DEBUG to turn (on)off debugging */
-//#define DEBUG  1
-#ifdef DEBUG
-#define PRINT(s)                                                               \
-  printf(s);                                                                   \
-  fflush(stdout)
-#define PRINT3(s, x, y, z)                                                     \
-  printf(s, x, y, z);                                                          \
-  fflush(stdout)
-#else
-#define PRINT(s)
-#define PRINT3(s, x, y, z)
-#endif
 
 // application-specific struct definition: needed for the visualizer
 struct appl_data {
@@ -84,88 +71,29 @@ int main(int argc, char *argv[]) {
   /* default values: */
   cols = DEFAULT_DIM;
   rows = DEFAULT_DIM;
-  iters = DEFAULT_INTERs;
+  iters = DEFAULT_INTERS;
   numtids = DEFAULT_TIDS;
 
-  /* (1) initalize application-specific state */
-  //     (a) parse command line arguments
-  if ((argc < 3) || (argc > 5)) {
-    printf("usage: ./pthr_simple [rows cols] [num_tids] [num_iters]\n");
-    exit(1);
-  }
-  if (argc >= 3) {
-    rows = atoi(argv[1]);
-    cols = atoi(argv[2]);
-  }
-  if (argc >= 4) {
-    numtids = atoi(argv[3]);
-  }
-  if (argc == 5) {
-    iters = atoi(argv[4]);
-  }
-  if (((rows % numtids) != 0) || ((cols % numtids) != 0)) {
-    printf("Error: number of threads %d must evenly divide dimensions %d:%d\n",
-           numtids, rows, cols);
-    exit(1);
-  }
-
-  //    (b) alloc arrays for threads
   all_ptids = malloc(sizeof(pthread_t) * numtids);
-  if (all_ptids == NULL) {
-    printf("ERROR malloc pthread array\n");
-    exit(1);
-  }
+  init_state(&data, rows, cols, numtids, iters);
 
-  //    (c) create and initialize program data
-  if (init_state(&data, rows, cols, numtids, iters) != 0) {
-    printf("ERROR init world\n");
-    exit(1);
-  }
-  printf("rows (height) = %d, cols (width) = %d thread %d iters %d\n", rows,
-         cols, numtids, iters);
-
-  // NOTE: (2) and (3) could be done in init_state but are here to
-  //       more clearly show the main steps for using pthreadgGridVisi lib
-  // (2) call init_pthread_animation
   myhandle = init_pthread_animation(numtids, rows, cols, visi_name);
-  if (myhandle == NULL) {
-    printf("ERROR init_pthread_animation\n");
-    exit(1);
-  }
+
   data.handle = myhandle;
-  // (3) call get_buff_pthread_animation
   data.image_buff = get_animation_buffer(myhandle);
-  if (data.image_buff == NULL) {
-    printf("ERROR get_buff_pthread_animation returned NULL\n");
-    exit(1);
-  }
-  PRINT("about to spawn worker threads...\n");
-  // (4) create worker threads passing each their thread-specific app data
   for (i = 0; i < numtids; i++) {
-    // TODO: memory leak at end?
     info = malloc(sizeof(struct appl_data));
-    if (!info) {
-      printf("ERROR malloc info\n");
-      exit(1);
-    }
     *info = data;    // init common fields
     info->mytid = i; // init thread specific fields
 
-    if (pthread_create(&all_ptids[i], NULL, thr_main, (void *)info)) {
-      printf("pthread_created failed on thread %d...handle this better\n", i);
-      exit(1);
-    }
+    pthread_create(&all_ptids[i], NULL, thr_main, (void *)info);
   }
-  // (5) call run_animation
+
   run_animation(myhandle, iters);
 
-  printf("main thread before calling pthread join\n");
-  fflush(stdout);
-  // (6) wait for all threads to exit
   for (i = 0; i < numtids; i++) {
     pthread_join(all_ptids[i], NULL);
   }
-  // then clean-up
   free(all_ptids);
 }
 /**********************************************************/
@@ -194,8 +122,7 @@ void *thr_main(void *args) {
   }
   // wait for all thread to init
   pthread_barrier_wait(data->done);
-  PRINT3("tid %d  start col = %d stop col = %d\n", data->mytid, start_c,
-         stop_c);
+
 
   if (iters == 0) {
     while (1) { /* run forever */
@@ -226,17 +153,9 @@ int init_state(struct appl_data *data, int r, int c, int t, int iters) {
   data->cols = c;
   data->numtids = t;
   data->grid = (int *)malloc(sizeof(int) * r * c);
-  if (!data->grid) {
-    printf("ERROR malloc\n");
-    return 1;
-  }
 
   data->done = (pthread_barrier_t *)malloc(sizeof(pthread_barrier_t));
   pthread_barrier_init(data->done, NULL, t);
-  if (!data->grid) {
-    printf("ERROR barrier malloc\n");
-    return 1;
-  }
 
   return 0;
 }
